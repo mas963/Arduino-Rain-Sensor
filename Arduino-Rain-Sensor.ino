08d978a62cf5ad5fbdd2f5c6f9ca981bc32598b5
mas963 <38841941+mas963@users.noreply.github.com>
2018-04-29 22:36:34 +0300

    Add files via upload

---
+++ Arduino-Rain-Sensor.ino
@@ -0,0 +1,33 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(7,6,5,4,3,2);
+int sensor = 8;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(sensor,INPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool okunan = digitalRead(sensor);
+  if(okunan == false){
+    Serial.println("yağmur var");
+    digitalWrite(9,HIGH);
+    digitalWrite(10,HIGH);
+    delay(1000);
+    digitalWrite(9,LOW);
+    digitalWrite(10,LOW);
+    lcd.begin(16,2);
+    lcd.clear();
+    lcd.print("Yagmur var");
+  }
+  else{
+    Serial.println("yağmur yok");
+    lcd.begin(16,2);
+    lcd.clear();
+    lcd.print("Yagmur yok");
+  }
+  delay(500);
+}
